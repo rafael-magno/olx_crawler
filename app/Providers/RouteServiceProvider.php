74b3e93c4cb476902b3e80c803fe4462cb0fea22
Rafael Magno <rafael.magno@monetizze.com.br>
2020-09-22 10:56:11 -0300

    fix(Remove prefixo api das rotas)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,11 +38,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-
-            Route::prefix('api')
-                ->middleware('api')
+            Route::middleware('api')
                 ->group(base_path('routes/api.php'));
         });
     }
